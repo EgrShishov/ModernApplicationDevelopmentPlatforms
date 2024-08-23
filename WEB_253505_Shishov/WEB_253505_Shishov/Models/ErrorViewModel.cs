57aae6fd27af4840bb7e2984179768e65d32e22f
EgrShishov <e.shishov98@yandex.ru>
2024-08-23 17:58:49 +0300

    Create project for first lab

---
+++ WEB_253505_Shishov/WEB_253505_Shishov/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace WEB_253505_Shishov.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
